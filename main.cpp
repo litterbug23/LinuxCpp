4a94e87c5a21ae93fdff8ff51394674a5e83d5fb
litterbug23 <litterbug23@163.com>
2022-07-19 18:01:33 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <experimental/filesystem>
+//c++ 11
+namespace fs = std::experimental::filesystem;
+
+std::wstring GetDefaultLogFile()
+{
+    fs::path logPath = fs::temp_directory_path();
+    logPath = logPath.parent_path().parent_path();
+    logPath.append("Glodon/GGP");
+    if (!fs::exists(logPath))
+    {
+        fs::create_directories(logPath);
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    struct tm tminfo;
+    localtime_s(&tminfo, &in_time_t);
+    char timebuffer[80];
+    std::strftime(timebuffer, 80, "GGP-%Y-%m-%d.txt", &tminfo);
+    logPath.append(timebuffer);
+
+    return logPath.c_str();
+}
+
+int main()
+{
+  std::wcout<<GetDefaultLogFile()<<std::endl;
+  return 1;
+}
